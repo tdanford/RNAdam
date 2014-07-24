607a813e1aed100b25d45afee7db3adada810bb8
Timothy Danford <tdanford@gmail.com>
2014-07-24 06:25:17 -0400

    Updating Classifier to use AlignmentRecord instead of ADAMRecord.

--- RNAdam-core/src/main/scala/org/bdgenomics/RNAdam/algorithms/defuse/Classifier.scala
+++ RNAdam-core/src/main/scala/org/bdgenomics/RNAdam/algorithms/defuse/Classifier.scala
@@ -19,24 +19,24 @@ package org.bdgenomics.RNAdam.algorithms.defuse
 
 import org.apache.spark.rdd.RDD
 import org.bdgenomics.RNAdam.models.ReadPair
-import org.bdgenomics.formats.avro.ADAMRecord
+import org.bdgenomics.formats.avro.AlignmentRecord
 
 object Classifier {
 
-  def classify(records: RDD[ADAMRecord]): (RDD[ReadPair], RDD[ReadPair], RDD[ReadPair]) = {
+  def classify(records: RDD[AlignmentRecord]): (RDD[ReadPair], RDD[ReadPair], RDD[ReadPair]) = {
 
-    val r1: RDD[(String, ADAMRecord)] = records.keyBy(x => x.getReadName.toString)
-    val aaa: RDD[(String, Iterable[(String, ADAMRecord)])] = r1.groupBy(p => p._1)
-    val bbb: RDD[(String, Seq[ADAMRecord])] = aaa.map {
-      case (key: String, iter: Iterable[(String, ADAMRecord)]) => (key, iter.map(x => x._2).toSeq)
+    val r1: RDD[(String, AlignmentRecord)] = records.keyBy(x => x.getReadName.toString)
+    val aaa: RDD[(String, Iterable[(String, AlignmentRecord)])] = r1.groupBy(p => p._1)
+    val bbb: RDD[(String, Seq[AlignmentRecord])] = aaa.map {
+      case (key: String, iter: Iterable[(String, AlignmentRecord)]) => (key, iter.map(x => x._2).toSeq)
     }
 
-    val groupedByReadName: RDD[(String, Seq[ADAMRecord])] = r1.groupBy(p => p._1).map {
-      case (key: String, iter: Iterable[(String, ADAMRecord)]) => (key, iter.map(x => x._2).toSeq)
+    val groupedByReadName: RDD[(String, Seq[AlignmentRecord])] = r1.groupBy(p => p._1).map {
+      case (key: String, iter: Iterable[(String, AlignmentRecord)]) => (key, iter.map(x => x._2).toSeq)
     }
 
     val readPairs: RDD[ReadPair] = groupedByReadName.flatMap {
-      case (key: String, records: Seq[ADAMRecord]) =>
+      case (key: String, records: Seq[AlignmentRecord]) =>
         findReadPairs(records)
     }
 
@@ -47,22 +47,22 @@ object Classifier {
     (concordant, spanning, split)
   }
 
-  protected def findReadPairs(records: Seq[ADAMRecord]): Seq[ReadPair] = {
+  protected def findReadPairs(records: Seq[AlignmentRecord]): Seq[ReadPair] = {
 
-    val firstRecords: Seq[ADAMRecord] = records.filter(_.getFirstOfPair)
-    val secondRecords: Seq[ADAMRecord] = records.filter(_.getSecondOfPair)
+    val firstRecords: Seq[AlignmentRecord] = records.filter(_.getFirstOfPair)
+    val secondRecords: Seq[AlignmentRecord] = records.filter(_.getSecondOfPair)
 
     firstRecords.flatMap {
-      case first: ADAMRecord =>
+      case first: AlignmentRecord =>
         secondRecords.map {
-          case second: ADAMRecord =>
+          case second: AlignmentRecord =>
             ReadPair(first, second)
         }
     }
   }
 
-  protected def hasTranscriptName(record: ADAMRecord): Boolean = {
-    record.getContig() != null
+  protected def hasTranscriptName(record: AlignmentRecord): Boolean = {
+    record.getContig != null
   }
 
   protected def sameTranscript(pair: ReadPair): Boolean = {
